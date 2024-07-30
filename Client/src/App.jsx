2256a2bfd18050f716cfb557661fc8849f9700b4
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-07-30 09:13:53 -0700

    create pages and routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Dashboard from './Pages/Dashboard'
+import Projects from './Pages/Projects'
 
 function App() {
  
 
   return (
-    <>
-     <p className='text-3xl font-bold'>I am Robiul Awal mohin</p>
-    </>
+   <BrowserRouter>
+          <Routes>
+             <Route path='/' element={<Home/>}/>
+             <Route path='/about' element={<About/>}/>
+             <Route path='/sign-in' element={<SignIn/>}/>
+             <Route path='/sign-up' element={<SignUp/>}/>
+             <Route path='/dashboard' element={<Dashboard/>}/>
+             <Route path='/project' element={<Projects/>}/>
+          </Routes>
+   </BrowserRouter>
   )
 }
 
