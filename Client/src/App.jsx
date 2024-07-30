5a39168a17a5295ab639030f06f609dfe1722746
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-07-29 20:45:20 -0700

    install react app and install tailwind  css and primary setup client side

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <p className='text-3xl font-bold'>I am Robiul Awal mohin</p>
+    </>
+  )
+}
+
+export default App
