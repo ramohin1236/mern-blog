ebee2ba3be080cac1e2be4cc8ce58270f2998bae
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-08-02 07:13:04 -0700

    sign up api routes

--- api/index.js
+++ api/index.js
@@ -2,26 +2,29 @@ import express from 'express';
 import mongoose  from 'mongoose';
 import dotenv from 'dotenv';
 import UserRoutes from './routes/user.route.js';
-const app =express();
+import AuthRoutes from './routes/auth.route.js';
 
 dotenv.config({ path: '../.env' });
-
+console.log(process.env.MONGODB_URL);
 // mongoose is connected with server
 mongoose
-.connect(process.env.MONGODB_URL)
-.then(
-    ()=>{console.log("Monogdb is Connected")}
-)
-.catch(err=>{
-    console.log("error connecting to-------->",err)
-})
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+const app =express();
+app.use(express.json());
 
 app.listen(3000,()=>{
     console.log("Server is runnig on port 3000!!");
 });
 
 app.use('/api/user',UserRoutes )
+app.use('/api/auth',AuthRoutes )
 
 
 
