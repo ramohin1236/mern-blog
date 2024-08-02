8cee3bc7f794d67ed1d507cda1490ce443833e77
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-08-01 20:55:14 -0700

    server side connect with mongoose

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 import express from 'express';
-
+import mongoose  from 'mongoose';
+import dotenv from 'dotenv';
 const app =express();
+
+dotenv.config({ path: '../.env' });
+
+// mongoose is connected with server
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(
+    ()=>{console.log("Monogdb is Connected")}
+)
+.catch(err=>{
+    console.log("error connecting to-------->",err)
+})
+
 
 app.listen(3000,()=>{
     console.log("Server is runnig on port 3000!!");
-})
+});
+
+
+
