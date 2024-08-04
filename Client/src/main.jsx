bc1c3ef897e9e95f1c5b886ad765b78ad77ed2d5
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-08-04 10:31:50 -0700

    add redux persist

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,12 +5,18 @@ import "./index.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <PersistGate persistor={persistor}>
     <Provider store={store}>
       <App />
       <ToastContainer />
     </Provider>
+    </PersistGate>
   </React.StrictMode>
 );
