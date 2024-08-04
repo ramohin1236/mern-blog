16124d908b23e6753687a7b6196f7c112d1907d0
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-08-04 03:40:09 -0700

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../model/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 // signup create--------------------------------
 export const signup =async (req,res,next)=>{
    const {username, email,password}=req.body;
@@ -29,4 +30,40 @@ export const signup =async (req,res,next)=>{
     next(error)
    }
 
+}
+
+// signin -----------------------------------
+
+export const signin =async(req,res,next)=>{
+  const {email, password}= req.body;
+
+  if(!email || !password || !email === ''|| !password===''){
+     next(errorHandler(400, "All fields are required"))
+  }
+
+  try{
+    // cheak valid user
+    const validUser = await User.findOne({email})
+    if(!validUser){
+       return next(errorHandler(400, "User not found!"))
+    }
+// cheak password correct
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if(!validPassword){
+      return  next(errorHandler(400, "Invalid password!"))
+    }
+// generate token
+    const token = jwt.sign(
+        { id: validUser._id},
+         process.env.JWT_SECTET,
+         {expiresIn: '365d'}
+    )
+    // hide password when login
+    const {password: pass, ...rest}= validUser._doc;
+  res.status(200).cookie('access_token', token,{
+    httpOnly:true}).json(rest);
+
+  }catch(error){
+    next(error)
+  }
 }
