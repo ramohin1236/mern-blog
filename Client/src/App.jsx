cd4f43ab8af0c840b64a6fc4dcd21f684cbd507f
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-08-06 21:07:29 -0700

    make private route

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,32 +1,31 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import SignIn from './Pages/SignIn'
-import SignUp from './Pages/SignUp'
-import Dashboard from './Pages/Dashboard'
-import Projects from './Pages/Projects'
-import Headers from './Components/Headers';
-import FooterComponent from './Components/FooterComponent'
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Dashboard from "./Pages/Dashboard";
+import Projects from "./Pages/Projects";
+import Headers from "./Components/Headers";
+import FooterComponent from "./Components/FooterComponent";
+import PrivateRoute from "./Components/PrivateRoute";
 
 function App() {
- 
-
   return (
-   <BrowserRouter>
-   <Headers/>
-          <Routes>
-             <Route path='/' element={<Home/>}/>
-             <Route path='/about' element={<About/>}/>
-             <Route path='/sign-in' element={<SignIn/>}/>
-             <Route path='/sign-up' element={<SignUp/>}/>
-             <Route path='/dashboard' element={<Dashboard/>}/>
-             <Route path='/project' element={<Projects/>}/>
-          </Routes>
-    <FooterComponent/>
-   </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Headers />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/project" element={<Projects />} />
+      </Routes>
+      <FooterComponent />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
