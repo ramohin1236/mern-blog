92b576c7f71af49f8a8c4de29bddb5507078dda7
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-08-06 22:15:29 -0700

    profile page ui created

--- Client/src/Components/DashProfile.jsx
+++ Client/src/Components/DashProfile.jsx
@@ -1,7 +1,56 @@
 
+import { Button, TextInput } from 'flowbite-react';
+import { useSelector } from 'react-redux';
 const DashProfile = () => {
+    const {currentUser}= useSelector(state=>state.user)
   return (
-    <div>DashProfile</div>
+    <div className='max-w-lg mx-auto p-3 w-full'>
+        <h1 className='my-7 text-center font-bold text-3xl'>{currentUser.username}</h1>
+       <form  className='flex flex-col gap-4'>
+        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
+        <img src={currentUser.profilePicture} 
+           alt='user_image'className='rounded-full w-full h-full border-8 object-cover  border-[lightgray]' />
+        </div>
+        {/* user name */}
+        <TextInput
+          type='text'
+          id='username'
+          placeholder='username'
+          defaultValue={currentUser.username}
+        
+        />
+        {/* user email */}
+        <TextInput
+          type='email'
+          id='email'
+          placeholder='email'
+          defaultValue={currentUser.email}
+         
+        />
+        {/* user password */}
+        <TextInput
+          type='password'
+          id='password'
+          placeholder='************'
+         
+        />
+
+<Button
+          type='submit'
+          gradientDuoTone='purpleToBlue'
+          outline
+        //   disabled={loading || imageFileUploading}
+        >
+          {/* {loading ? 'Loading...' : 'Update'} */}
+          submit
+        </Button>
+       </form>
+       <div>
+       <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+              Are you sure you want to delete your account?
+            </h3>
+       </div>
+    </div>
   )
 }
 
