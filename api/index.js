c586136065cba2253719608a7102f2cff18bf639
ramohin1236 <mohinr26@gmail.com>
2025-05-05 00:49:08 +0600

    Fix: Corrected static path for client build

--- api/index.js
+++ api/index.js
@@ -41,17 +41,23 @@ app.use('/api/comment',CommentRoutes)
 
 
 // app.use(express.static(path.join(__dirname, '/client/dist')));
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.use(express.static(path.join(__dirname, '/client/dist')));
 
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 // });
 
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// });
+
+// Serve static files from React
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Catch-all to serve index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
-
-
 // middleware
 app.use((err, req, res, next)=>{
     
